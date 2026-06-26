ff1c3bc2b25be7eb9b961bde4e026aa6ee2f8b2d
hhnam746 <dafox746@gmail.com>
2026-06-26 08:42:19 +0700

    chores: invalidate signalR in cancel booking for test process

--- SWP391-AutoWashPro-BE.Service/Booking/Service.cs
+++ SWP391-AutoWashPro-BE.Service/Booking/Service.cs
@@ -1030,14 +1030,14 @@ public class Service : IService
             IsRead = false,
             CreatedAt = DateTimeOffset.UtcNow,
         };
-        await _notificationService.SendNotification(new Notification.Request.SendNotificationRequest
-        {
-            UserId = userIdGuid,
-            Type = NotificationType.BookingCreated,
-            Data = $"Your booking at {branch?.Name ?? "our branch"} " +
-                   $"for {booking.StartTime:HH:mm dd/MM/yyyy} " +
-                   $"has been cancelled successfully.",
-        });
+        // await _notificationService.SendNotification(new Notification.Request.SendNotificationRequest
+        // {
+        //     UserId = userIdGuid,
+        //     Type = NotificationType.BookingCancelled,
+        //     Data = $"Your booking at {branch?.Name ?? "our branch"} " +
+        //            $"for {booking.StartTime:HH:mm dd/MM/yyyy} " +
+        //            $"has been cancelled successfully.",
+        // });
 
         _dbContext.Notifications.Add(notification);
 
