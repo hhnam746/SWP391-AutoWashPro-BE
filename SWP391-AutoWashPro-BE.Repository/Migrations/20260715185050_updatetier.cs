6850ec623b36a0621c77bf6101eca542edfee430
tranminhthien-tech <tranminhthien188@gmail.com>
2026-07-16 01:55:17 +0700

    update db

---
+++ SWP391-AutoWashPro-BE.Repository/Migrations/20260715185050_updatetier.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SWP391_AutoWashPro_BE.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatetier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_tier_level",
+                table: "tier");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tier_name",
+                table: "tier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tier_level",
+                table: "tier",
+                column: "level",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tier_name",
+                table: "tier",
+                column: "name",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_tier_level",
+                table: "tier");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tier_name",
+                table: "tier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tier_level",
+                table: "tier",
+                column: "level",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tier_name",
+                table: "tier",
+                column: "name",
+                unique: true);
+        }
+    }
+}
