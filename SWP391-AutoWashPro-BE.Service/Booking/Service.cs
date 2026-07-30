a7243524475f19fce023fa05e74df30ef10efafa
hhnam746 <dafox746@gmail.com>
2026-07-30 21:19:23 +0700

    bugfix/add filter constraint

--- SWP391-AutoWashPro-BE.Service/Booking/Service.cs
+++ SWP391-AutoWashPro-BE.Service/Booking/Service.cs
@@ -387,7 +387,9 @@ public class Service : IService
                 .Include(x => x.PersonalizedVoucherIssuance)
                 .FirstOrDefaultAsync(x =>
                     x.Id == bookingRequest.VoucherId.Value &&
-                    x.CustomerId == customerProfile.Id);
+                    x.CustomerId == customerProfile.Id && 
+                    x.Status != VoucherStatus.Used && 
+                    x.Status != VoucherStatus.Expired);
 
             if (voucher == null)
                 throw new Exception("Voucher not found");
@@ -416,6 +418,7 @@ public class Service : IService
             if (voucher.DiscountType == DiscountType.FixedAmount)
             {
                 voucherDiscountAmount += voucher.DiscountValue;
+               
             }
             else
             {
